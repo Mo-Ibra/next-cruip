86873f973de5eeb1e66da503c0327badc36e8bd6
Dev-Maradona <maradona.0000@yahoo.com>
2022-08-13 09:44:20 -0700

    Added WorkFlow Section

---
+++ pages/blog.tsx
@@ -0,0 +1,11 @@
+import WorkFlow from "../sections/word-flow/work-flow";
+
+const Blog = () => {
+    return (
+        <>
+            <WorkFlow />
+        </>
+    )
+}
+
+export default Blog;
---
+++ sections/word-flow/work-flow.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+
+import MainImg from '../../assets/images/blog/main.jpg';
+
+import AvatarImg from '../../assets/images/avatars/1.jpg';
+
+import RightSvg from "../../components/header/svg/right-svg";
+
+const WorkFlow = () => {
+    return (
+        <section className="py-20 relative">
+            <div className="container mx-auto px-10">
+                <h2 className="header-color text-3xl md:text-3xl lg:text-5xl font-bold w-2/4 mb-10">Refreshing news for developers and designers</h2>
+                <div className="grid md:grid-cols-1 lg:grid-cols-2 gap-10 my-10">
+                    <div>
+                        <Image src={MainImg} width="520px" height="290px" />
+                    </div>
+                    <div>
+                        <div className="my-5">
+                            <span className="bg-blue-500 text-white py-1 px-2 text-sm mr-2 rounded-full">Product</span>
+                            <span className="bg-red-500 text-white py-1 px-2 text-sm mr-2 rounded-full">Engineering</span>
+                        </div>
+                        <div>
+                            <h3 className="header-color my-2 text-3xl font-bold">Designing a functional workflow at home.</h3>
+                            <p className="text-color my-2">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <div className="my-5">
+                                <Image src={AvatarImg} width="48px" height="48px" className="rounded-full" />
+                            </div>
+                            <p className="text-lg text-color">Chris Solerieu - Jan 19, 2020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='absolute top-0 right-0 -z-10'>
+                <RightSvg />
+            </div>
+        </section>
+    )
+}
+
+export default WorkFlow;
