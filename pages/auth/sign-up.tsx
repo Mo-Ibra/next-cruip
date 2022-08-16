43624fc40c6b31429ac667a7791cc91a1b4576c0
Dev-Maradona <maradona.0000@yahoo.com>
2022-08-15 23:31:14 -0700

    Added Sign Up Page.

---
+++ pages/auth/sign-up.tsx
@@ -0,0 +1,15 @@
+import SignUpSection from '../../sections/auth/sign-up-section/sign-up-section';
+import Footer from "../../components/footer/Footer";
+import Hello from '../../sections/hello/hello';
+
+const SignIn = () => {
+    return (
+        <>
+            <SignUpSection />
+            <Hello />
+            <Footer />
+        </>
+    )
+}
+
+export default SignIn;
--- sections/auth/sign-up-section/sign-up-section.tsx
+++ sections/auth/sign-up-section/sign-up-section.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import RightSvg from "../../../components/header/svg/right-svg";
 
-const SignInSection = () => {
+const SignUpSection = () => {
     return (
         <section className="py-10 relative">
             <div className="container mx-auto px-10">
@@ -9,29 +9,21 @@ const SignInSection = () => {
                     <h2 className="header-color text-2xl md:text-3xl lg:text-5xl font-bold mx-auto w-3/4">Welcome back. We exist to make entrepreneurship easier.</h2>
                 </div>
                 <div className="mt-14 text-center md:w-2/4 lg:w-1/3 mx-auto">
-                    <button className="bg-red-600 text-white p-2 w-full font-semibold">Sign In with Google</button>
+                    <button className="bg-red-600 text-white p-2 w-full font-semibold">Sign Up with Google</button>
                     <div className="border-b border-gray-800 mb-4" />
                     <form>
+                        <input type="text" placeholder="Enter your name!" className="bg-inherit text-color border border-gray-700 outline-none p-2 w-full my-3" />
+                        <input type="text" placeholder="Enter your company!" className="bg-inherit text-color border border-gray-700 outline-none p-2 w-full my-3" />
                         <input type="text" placeholder="Enter your email!" className="bg-inherit text-color border border-gray-700 outline-none p-2 w-full my-3" />
                         <input type="text" placeholder="Enter your password" className="bg-inherit text-color border border-gray-700 outline-none p-2 w-full my-3" />
                         <div className="flex items-center justify-between my-3">
-                            <div className="flex items-center gap-2">
-                                <input type="checkbox" id="check" />
-                                <label htmlFor="check" className="text-color">Keep me login</label>
-                            </div>
-                            <div>
-                                <p className="text-main font-bold cursor-pointer">Forgot Password?</p>
-                            </div>
+                            <p className="text-color">I agree to be contacted by Open PRO about this offer as per the Open PRO <span className="underline">Privacy Policy</span>.</p>
                         </div>
                         <div className="my-5">
-                            <button className="bg-main text-white w-full p-2">Sign In</button>
+                            <button className="bg-main text-white w-full p-2">Sign Up</button>
                         </div>
                         <div>
-                            <p className="text-color">Don’t you have an account?
-                                <Link href="/auth/sign-up">
-                                    <span className="text-main font-bold cursor-pointer">Sign up</span>
-                                </Link>
-                            </p>
+                            <p className="text-color">Already have an account? <Link href="/auth/sign-in"><span className="text-main font-bold cursor-pointer">Sign In</span></Link></p>
                         </div>
                     </form>
                 </div>
@@ -43,4 +35,4 @@ const SignInSection = () => {
     )
 }
 
-export default SignInSection;
+export default SignUpSection;
